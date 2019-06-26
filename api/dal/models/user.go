70d4b4cee3ff3f727563f6effe198ab38c600259
Tracey Eubanks <tracey@outdoorsy.co>
2019-06-26 11:07:13 -0400

    started completing the store

--- api/dal/models/user.go
+++ api/dal/models/user.go
@@ -1,3 +1,10 @@
 package models
 
-type User struct{}
+type User struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Email     string
+	Age       int
+	Birthday  string
+}
--- api/dal/store/user.go
+++ api/dal/store/user.go
@@ -2,9 +2,16 @@ package store
 
 import "github.com/outdoorsy/api/model"
 
+var (
+	getUsersStmt = "SELECT * FROM users"
+ insertUserStmt = "INSERT INTO users (first_name, last_name, email, age, birthday) VALUSE (:first_name, :last_name, :email, :age, :birthday)"
+deleteUserStmt = "DELETE FROM users WHERE id=?"
+)
 // ReadUsers queries the database for all users
 func (s *Store) ReadUsers() ([]*model.User, error) {
-	return []*model.User{}, nil
+	var users []*model.User{}
+	err := s.dbConn.Select(users, getUsersStmt)
+	return users, err
 }
 
 // ReadUser queries the database for the user that exists for the passed in ID
@@ -23,12 +30,23 @@ func (s *Store) ReadUserByEmail(email string) (*model.User, error) {
 }
 
 func (s *Store) readUser(filters map[string]interface{}) (*model.User, error) {
+	var user *model.User
+
+	clauses := []string{}
+	for k, v := range filters {
+		clauses := fmt.Sprintf("k=?")
+	}
+	if len(clauses) == 0 {
+		return user, nil
+	}
+
+	query := fmt.Sprintf(getUsersStmt + " WHERE "
 	return &model.User{}, nil
 }
 
 // CreateUser inserts a new user into the system
 func (s *Store) CreateUser(u *model.User) error {
-	return nil
+	return s.dbConn.NamedExec(insertUserStmt, u)
 }
 
 // UpdateUser updates the passed in model.User using the members of the passed
@@ -38,6 +56,6 @@ func (s *Store) UpdateUser(u *model.User) error {
 }
 
 // DeleteUser deletes the user that has the specified ID
-func (s *Store) DeleteUser(id int) error {
-	return nil
+func (s *Store) DeleteUser(u *model.User) error {
+	return s.dbConn.Exec(deleteUserStmt, u.ID)
 }
