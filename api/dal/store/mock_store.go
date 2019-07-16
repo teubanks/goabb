4f7d26b96fc011c1972a6db8b37aa63cf1e9b681
Tracey Eubanks <tracey@outdoorsy.co>
2019-07-16 11:43:33 -0400

    models not model :facepalm:

--- api/dal/store/mock_store.go
+++ api/dal/store/mock_store.go
@@ -1,50 +1,50 @@
 package store
 
-import "github.com/outdoorsy/api/model"
+import "github.com/teubanks/goabb/api/dal/models"
 
 // MockStore should be used in tests only
 type MockStore struct {
-	OnReadUsers       func() ([]*model.User, error)
-	OnReadUser        func(int) (*model.User, error)
-	OnReadUserByEmail func(string) (*model.User, error)
-	OnCreateUser      func(*model.User) error
-	OnUpdateUser      func(*model.User) error
+	OnReadUsers       func() ([]*models.User, error)
+	OnReadUser        func(int) (*models.User, error)
+	OnReadUserByEmail func(string) (*models.User, error)
+	OnCreateUser      func(*models.User) error
+	OnUpdateUser      func(*models.User) error
 	OnDeleteUser      func(int) error
 }
 
 // ReadUsers will return the results of OnReadUsers if set. Otherwise, an empty
-// slice of model.Users and a nil error will be returned
-func (m *MockStore) ReadUsers() ([]*model.User, error) {
+// slice of models.Users and a nil error will be returned
+func (m *MockStore) ReadUsers() ([]*models.User, error) {
 	if m.OnReadUsers != nil {
 		return m.OnReadUsers()
 	}
 
-	return []*model.User{}, nil
+	return []*models.User{}, nil
 }
 
 // ReadUser will return the results of OnReadUser, if set. Otherwise, an empty
-// model.User struct and a nil error will be returned
-func (m *MockStore) ReadUser(id int) (*model.User, error) {
+// models.User struct and a nil error will be returned
+func (m *MockStore) ReadUser(id int) (*models.User, error) {
 	if m.OnReadUser != nil {
 		return m.OnReadUser(id)
 	}
 
-	return &model.User{}, nil
+	return &models.User{}, nil
 }
 
 // ReadUserByEmail will return the results of OnReadUserByEmail, if set.
-// Otherwise, an empty model.User struct and a nil error will be returned
-func (m *MockStore) ReadUserByEmail(email string) (*model.User, error) {
+// Otherwise, an empty models.User struct and a nil error will be returned
+func (m *MockStore) ReadUserByEmail(email string) (*models.User, error) {
 	if m.OnReadUserByEmail != nil {
 		return m.OnReadUserByEmail(email)
 	}
 
-	return &model.User{}, nil
+	return &models.User{}, nil
 }
 
 // CreateUser will return the result of OnCreateUser if set, otherwise, a nil
 // error is returned
-func (m *MockStore) CreateUser(u *model.User) error {
+func (m *MockStore) CreateUser(u *models.User) error {
 	if m.OnCreateUser != nil {
 		return m.OnCreateUser(u)
 	}
@@ -53,7 +53,7 @@ func (m *MockStore) CreateUser(u *model.User) error {
 
 // UpdateUser will return the result of OnUpdateUser if set, otherwise, a nil
 // error is returned
-func (m *MockStore) UpdateUser(u *model.User) error {
+func (m *MockStore) UpdateUser(u *models.User) error {
 	if m.OnUpdateUser != nil {
 		return m.OnUpdateUser(u)
 	}
