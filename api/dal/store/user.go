939c556d1df97fd63ddd36af3c794a1cdda073ff
Tracey Eubanks <tracey@outdoorsy.co>
2019-07-16 11:42:53 -0400

    fixed some compile issues

--- api/dal/store/user.go
+++ api/dal/store/user.go
@@ -1,21 +1,26 @@
 package store
 
-import "github.com/outdoorsy/api/model"
+import (
+	"fmt"
+
+	"github.com/teubanks/goabb/api/dal/models"
+)
 
 var (
-	getUsersStmt = "SELECT * FROM users"
- insertUserStmt = "INSERT INTO users (first_name, last_name, email, age, birthday) VALUSE (:first_name, :last_name, :email, :age, :birthday)"
-deleteUserStmt = "DELETE FROM users WHERE id=?"
+	getUsersStmt   = "SELECT * FROM users"
+	insertUserStmt = "INSERT INTO users (first_name, last_name, email, age, birthday) VALUSE (:first_name, :last_name, :email, :age, :birthday)"
+	deleteUserStmt = "DELETE FROM users WHERE id=?"
 )
+
 // ReadUsers queries the database for all users
-func (s *Store) ReadUsers() ([]*model.User, error) {
-	var users []*model.User{}
+func (s *Store) ReadUsers() ([]*models.User, error) {
+	var users []*models.User
 	err := s.dbConn.Select(users, getUsersStmt)
 	return users, err
 }
 
 // ReadUser queries the database for the user that exists for the passed in ID
-func (s *Store) ReadUser(id int) (*model.User, error) {
+func (s *Store) ReadUser(id int) (*models.User, error) {
 	filters := make(Filters)
 	filters["id"] = id
 	return s.readUser(filters)
@@ -23,14 +28,14 @@ func (s *Store) ReadUser(id int) (*model.User, error) {
 
 // ReadUserByEmail queries the database for the user that exists with the
 // specified email
-func (s *Store) ReadUserByEmail(email string) (*model.User, error) {
+func (s *Store) ReadUserByEmail(email string) (*models.User, error) {
 	filters := make(Filters)
 	filters["email"] = email
 	return s.readUser(filters)
 }
 
-func (s *Store) readUser(filters map[string]interface{}) (*model.User, error) {
-	var user *model.User
+func (s *Store) readUser(filters map[string]interface{}) (*models.User, error) {
+	var user *models.User
 
 	clauses := []string{}
 	for k, v := range filters {
@@ -40,22 +45,22 @@ func (s *Store) readUser(filters map[string]interface{}) (*model.User, error) {
 		return user, nil
 	}
 
-	query := fmt.Sprintf(getUsersStmt + " WHERE "
-	return &model.User{}, nil
+	// query := fmt.Sprintf(getUsersStmt + " WHERE "
+	return &models.User{}, nil
 }
 
 // CreateUser inserts a new user into the system
-func (s *Store) CreateUser(u *model.User) error {
+func (s *Store) CreateUser(u *models.User) error {
 	return s.dbConn.NamedExec(insertUserStmt, u)
 }
 
-// UpdateUser updates the passed in model.User using the members of the passed
+// UpdateUser updates the passed in models.User using the members of the passed
 // in struct as attributes
-func (s *Store) UpdateUser(u *model.User) error {
+func (s *Store) UpdateUser(u *models.User) error {
 	return nil
 }
 
 // DeleteUser deletes the user that has the specified ID
-func (s *Store) DeleteUser(u *model.User) error {
+func (s *Store) DeleteUser(u *models.User) error {
 	return s.dbConn.Exec(deleteUserStmt, u.ID)
 }
